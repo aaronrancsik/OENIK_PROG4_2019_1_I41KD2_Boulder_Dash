afccda92dabda34d904f24e02a5c1b83dfaab7ab
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-03-25 20:50:31 +0100

    add initial static block

---
+++ NIK.BoulderDash.Logic/StaticBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIK.BoulderDash.Logic
+{
+    class StaticBlock
+    {
+        int posX;
+        int posY;
+    }
+}
