e39e3b17cebbda3099c0ac4cac0a83f9d3dfebc3
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-03-25 20:43:46 +0100

    Add initial player interface

---
+++ NIK.BoulderDash.Logic/IPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIK.BoulderDash.Logic
+{
+    interface IPlayer
+    {
+        int PosX { get; }
+        int PosY { get; }
+
+        void MoveLeft();
+        void MoveRight();
+        void MoveUp();
+        void MoveDown();
+
+
+    }
+}
