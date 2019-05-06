a7c612f4cf69cf444feb90e5be27ac3b9d845b16
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-07 00:48:53 +0200

    add dirt

---
+++ NIK.BoulderDash.Logic/Blocks/Dirt.cs
@@ -0,0 +1,14 @@
+﻿using NIK.BoulderDash.Logic.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIK.BoulderDash.Logic
+{
+    public class Dirt : IVariable
+    {
+        public int Variant { get; set; }
+    }
+}
