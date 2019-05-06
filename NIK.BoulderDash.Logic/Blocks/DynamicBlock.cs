fa90c3a2d0ff3553848048a9d9f6b081f77baaa7
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-07 00:52:35 +0200

    add DynamicBlock

---
+++ NIK.BoulderDash.Logic/Blocks/DynamicBlock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NIK.BoulderDash.Logic
+{
+    public class DynamicBlock: Block
+    {
+        public ref Point tileOldPosition { get => ref tilePosition; }
+
+    }
+}
