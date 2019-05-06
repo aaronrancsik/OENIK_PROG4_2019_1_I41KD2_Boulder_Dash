ca5f569be3f7de38a4745060f06639322c69a570
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-07 00:54:21 +0200

    the initial game model

--- NIK.BoulderDash.Logic/GameModel.cs
+++ NIK.BoulderDash.Logic/GameModel.cs
@@ -4,23 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-
+using NIK.BoulderDash.Logic.Blocks;
 namespace NIK.BoulderDash.Logic
 {
-    public class GameModel : IGameModel
+    public class GameModel 
     {
         public GameModel()
         {
-            Player = new Player(new Point(10,10));
+           
         }
-        public int Time { get; private set; }
-
+        public int CollectedDiamonds { get; set; };
+        public int RequireDiamonds { get; set; }
+        public Point ExitPistition { get; set; }
+        
+        public List<Diamond> Diamonds { get; private set; }
+        public List<Boulder> Boulders { get; private set; }
+        public Dirt[,] DirtMatrix { get; set; }
+        public bool[,] TitaniumMatrix { get; set; }
+        public bool[,] WallMatrix { get; set; }
         public Player Player { get; private set; }
-
-        public Map Map { get; private set; }
-
-        public Camera Camera { get; private set; }
-
-        public List<Enemie> Enemies { get; private set; }
     }
 }
