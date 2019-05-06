e8db15349af05f27bab2fb973e4654c99a7e6c95
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-07 00:48:43 +0200

    add block

---
+++ NIK.BoulderDash.Logic/Blocks/Block.cs
@@ -0,0 +1,17 @@
+﻿using NIK.BoulderDash.Logic.Blocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NIK.BoulderDash.Logic
+{
+    public class Block : IVariable
+    {
+        public ref Point tilePosition { get => ref tilePosition; }
+        public static int Set { get; set; }
+        public int Variant { get; set; }
+    }
+}
