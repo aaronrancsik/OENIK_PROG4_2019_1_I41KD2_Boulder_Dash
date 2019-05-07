882fb9bd45b6d453fec19bc0a131f4359ba9e5da
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-07 20:40:01 +0200

    dummy classes for map matrix

---
+++ NIK.BoulderDash.Logic/Blocks/Titanium.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NIK.BoulderDash.Logic.Blocks
+{
+    class Titanium: Block, IVariable
+    {
+        public int Variant { get; set; }
+    }
+}
