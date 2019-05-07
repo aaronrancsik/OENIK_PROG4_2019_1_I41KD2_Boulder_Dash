384b0610e1e345c31c1e5a5a0c40cf4086ce70f0
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-07 20:46:46 +0200

    vs generated

--- NIK.BoulderDash.UI/Properties/Resources.Designer.cs
+++ NIK.BoulderDash.UI/Properties/Resources.Designer.cs
@@ -431,6 +431,16 @@ namespace NIK.BoulderDash.UI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] AL02 {
+            get {
+                object obj = ResourceManager.GetObject("AL02", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Boulder1 {
