68a9b2540d92a24e96fe64146d7c0baa7984c91e
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 19:48:20 +0200

    frefly test map

--- NIK.BoulderDash.UI/Properties/Resources.Designer.cs
+++ NIK.BoulderDash.UI/Properties/Resources.Designer.cs
@@ -461,6 +461,16 @@ namespace NIK.BoulderDash.UI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] AL10test {
+            get {
+                object obj = ResourceManager.GetObject("AL10test", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Boulder1 {
