939e09cf438b77e170b03b4dca225b5627c55f7f
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 16:09:02 +0200

    add enemie base

--- NIK.BoulderDash.Logic/Enemie.cs
+++ NIK.BoulderDash.Logic/Enemie.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NIK.BoulderDash.Logic.Blocks;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,11 @@ using System.Threading.Tasks;
 
 namespace NIK.BoulderDash.Logic
 {
-    public class Enemie
+    public abstract class Enemie : DynamicBlock
     {
         public Enemie() : base(true, false)
         {
         }
+        virtual public void Step(bool[,] obstacle) { }
     }
 }
