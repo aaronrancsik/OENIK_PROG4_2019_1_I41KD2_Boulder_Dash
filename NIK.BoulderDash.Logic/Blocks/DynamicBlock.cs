7778d6df7ff69ac12f41fa463b45a6926aece332
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 15:26:53 +0200

    make full prop for later layering

--- NIK.BoulderDash.Logic/Blocks/DynamicBlock.cs
+++ NIK.BoulderDash.Logic/Blocks/DynamicBlock.cs
@@ -9,15 +9,24 @@ namespace NIK.BoulderDash.Logic.Blocks
 {
     public class DynamicBlock: Block
     {
-        private Point tilePosition;
-        public ref Point TilePosition { get => ref tilePosition; }
-
         public DynamicBlock(bool explosive, bool isRounded) :base(isRounded)
         {
             this.Explosive = explosive;
         }
         private Point tileOldPosition;
         public ref Point TileOldPosition { get => ref tileOldPosition; }
+        private Point tilePosition;
+        public ref Point TilePosition
+        {
+            get
+            {
+                //tileOldPosition.X = tilePosition.X;
+                //tileOldPosition.Y = tilePosition.Y;
+                return ref tilePosition;
+            }
+        }
+
+        
 
     }
 }
