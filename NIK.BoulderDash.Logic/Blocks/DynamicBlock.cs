309cb313258a07532142a5653a7cf6cf61f854a7
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 15:42:34 +0200

    add falling prop

--- NIK.BoulderDash.Logic/Blocks/DynamicBlock.cs
+++ NIK.BoulderDash.Logic/Blocks/DynamicBlock.cs
@@ -14,6 +14,7 @@ namespace NIK.BoulderDash.Logic.Blocks
             this.Explosive = explosive;
         }
         public bool Explosive { get; private set; }
+        public bool Falling { get; set; }
         private Point tileOldPosition;
         public ref Point TileOldPosition { get => ref tileOldPosition; }
         private Point tilePosition;
