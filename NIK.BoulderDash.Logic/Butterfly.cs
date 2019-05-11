c6e6b44194e959133a226b939b8f3d27253423e6
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-12 01:06:46 +0200

    add new butterfly enemy

---
+++ NIK.BoulderDash.Logic/Butterfly.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NIK.BoulderDash.Logic
+{
+    public class Butterfly : Enemie
+    {
+        public Butterfly(Point initPosition)
+        {
+            TilePosition = initPosition;
+            TileOldPosition = initPosition;
+            FaceDirection = Direction.Down;
+        }
+
+
+        public override void Step(bool[,] obstacle)
+        {
+            var primTarget = calcUnit(Direction.Right);
+            var secTarget = calcUnit(Direction.Up);
+            if (!obstacle[(int)primTarget.X, (int)primTarget.Y])
+            {
+                FaceDirection = GetRight();
+                move(primTarget);
+            }
+            else if (!obstacle[(int)secTarget.X, (int)secTarget.Y])
+            {
+                move(secTarget);
+            }
+            else
+            {
+                FaceDirection = GetLeft();
+            }
+
+        }
+    }
+}
