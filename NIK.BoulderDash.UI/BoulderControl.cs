443dea6048bc3d7643514d48b2faccf686e9fff7
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-12 01:06:56 +0200

    load new map

--- NIK.BoulderDash.UI/BoulderControl.cs
+++ NIK.BoulderDash.UI/BoulderControl.cs
@@ -53,7 +53,7 @@ namespace NIK.BoulderDash.UI
             }
 
             logic = new GameLogic();
-            model = logic.LoadLevel(Properties.Resources.AL10);
+            model = logic.LoadLevel(Properties.Resources.AL14);
             display = new BoulderDisplay(model, ActualWidth, ActualHeight, GameModel.MOVETIME, animatedVisualBrushes);
             InvalidateVisual();
         }
