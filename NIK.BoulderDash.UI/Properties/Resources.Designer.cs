cec2510f92825769b139cc7d50a829b3a6663381
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-12 01:05:57 +0200

    add new butterflymap

--- NIK.BoulderDash.UI/Properties/Resources.Designer.cs
+++ NIK.BoulderDash.UI/Properties/Resources.Designer.cs
@@ -471,6 +471,16 @@ namespace NIK.BoulderDash.UI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] AL14 {
+            get {
+                object obj = ResourceManager.GetObject("AL14", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Boulder1 {
