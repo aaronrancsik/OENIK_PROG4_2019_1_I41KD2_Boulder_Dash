a88b66d63952c4293a9eca09ffa654ad12e209cf
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 15:38:45 +0200

    add level resource

--- NIK.BoulderDash.UI/Properties/Resources.Designer.cs
+++ NIK.BoulderDash.UI/Properties/Resources.Designer.cs
@@ -441,6 +441,26 @@ namespace NIK.BoulderDash.UI.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] AL03 {
+            get {
+                object obj = ResourceManager.GetObject("AL03", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] AL10 {
+            get {
+                object obj = ResourceManager.GetObject("AL10", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap Boulder1 {
