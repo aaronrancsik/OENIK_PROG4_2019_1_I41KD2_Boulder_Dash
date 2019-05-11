62fde25e2a16144713730286983065848db97142
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 23:41:46 +0200

    extract  firefile reuseable code to base

--- NIK.BoulderDash.Logic/Enemie.cs
+++ NIK.BoulderDash.Logic/Enemie.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace NIK.BoulderDash.Logic
 {
@@ -12,6 +13,123 @@ namespace NIK.BoulderDash.Logic
         public Enemie() : base(true, false)
         {
         }
+        protected Direction FaceDirection { get; set; }
         virtual public void Step(bool[,] obstacle) { }
+
+        protected void move(Point p)
+        {
+            TileOldPosition = TilePosition;
+            TilePosition.X = p.X;
+            TilePosition.Y = p.Y;
+        }
+
+        protected Direction GetLeft()
+        {
+            if (FaceDirection == Direction.Left)
+                return Direction.Down;
+            else if (FaceDirection == Direction.Down)
+                return Direction.Right;
+            else if (FaceDirection == Direction.Right)
+                return Direction.Up;
+            else if (FaceDirection == Direction.Up)
+                return Direction.Left;
+            throw new Exception("Bad direction");
+        }
+        protected Direction GetRight()
+        {
+            if (FaceDirection == Direction.Left)
+                return Direction.Up;
+            else if (FaceDirection == Direction.Down)
+                return Direction.Left;
+            else if (FaceDirection == Direction.Right)
+                return Direction.Down;
+            else if (FaceDirection == Direction.Up)
+                return Direction.Right;
+            throw new Exception("Bad direction");
+        }
+        protected Point calcUnit(Direction dir)
+        {
+            if (FaceDirection == Direction.Up)
+            {
+                if (dir == Direction.Left)
+                {
+                    return new Point(TilePosition.X - 1, TilePosition.Y);
+                }
+                else if (dir == Direction.Right)
+                {
+                    return new Point(TilePosition.X + 1, TilePosition.Y);
+                }
+                else if (dir == Direction.Up)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y - 1);
+                }
+                else if (dir == Direction.Down)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y + 1);
+                }
+            }
+            else if (FaceDirection == Direction.Down)
+            {
+                if (dir == Direction.Left)
+                {
+                    return new Point(TilePosition.X + 1, TilePosition.Y);
+
+                }
+                else if (dir == Direction.Right)
+                {
+                    return new Point(TilePosition.X - 1, TilePosition.Y);
+                }
+                else if (dir == Direction.Up)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y + 1);
+                }
+                else if (dir == Direction.Down)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y - 1);
+                }
+            }
+            else if (FaceDirection == Direction.Left)
+            {
+                if (dir == Direction.Left)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y + 1);
+                }
+                else if (dir == Direction.Right)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y - 1);
+
+                }
+                else if (dir == Direction.Up)
+                {
+                    return new Point(TilePosition.X - 1, TilePosition.Y);
+
+                }
+                else if (dir == Direction.Down)
+                {
+                    return new Point(TilePosition.X + 1, TilePosition.Y);
+                }
+            }
+            else if (FaceDirection == Direction.Right)
+            {
+                if (dir == Direction.Left)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y - 1);
+                }
+                else if (dir == Direction.Right)
+                {
+                    return new Point(TilePosition.X, TilePosition.Y + 1);
+                }
+                else if (dir == Direction.Up)
+                {
+                    return new Point(TilePosition.X + 1, TilePosition.Y);
+                }
+                else if (dir == Direction.Down)
+                {
+                    return new Point(TilePosition.X - 1, TilePosition.Y);
+                }
+            }
+            return new Point(0, 0);
+
+        }
     }
 }
--- NIK.BoulderDash.Logic/Firefly.cs
+++ NIK.BoulderDash.Logic/Firefly.cs
@@ -14,143 +14,16 @@ namespace NIK.BoulderDash.Logic
         {
             TilePosition = initPosition;
             TileOldPosition = initPosition;
+            FaceDirection = Direction.Left;
         }
 
-        Direction faceDirection= Direction.Down;
-        private Direction GetLeft()
-        {
-            if (faceDirection == Direction.Left)
-                return Direction.Down;
-            else if(faceDirection==Direction.Down)
-                return Direction.Right;
-            else if (faceDirection == Direction.Right)
-                return Direction.Up;
-            else if (faceDirection == Direction.Up)
-                return Direction.Left;
-            throw new Exception("Bad direction");
-        }
-        private Direction GetRight()
-        {
-            if (faceDirection == Direction.Left)
-                return Direction.Up;
-            else if (faceDirection == Direction.Down)
-                return Direction.Left;
-            else if (faceDirection == Direction.Right)
-                return Direction.Down;
-            else if (faceDirection == Direction.Up)
-                return Direction.Right;
-            throw new Exception("Bad direction");
-        }
 
-        public Point unit(Direction dir)
-        {
-            if(faceDirection == Direction.Up)
-            {
-                if (dir == Direction.Left)
-                {
-                    return new Point(TilePosition.X - 1, TilePosition.Y);
-                }
-                else if(dir == Direction.Right)
-                {
-                    return new Point(TilePosition.X + 1, TilePosition.Y);
-                }
-                else if (dir == Direction.Up)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y-1);
-                }
-                else if (dir == Direction.Down)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y+1);
-                }
-            }
-            else if(faceDirection == Direction.Down)
-            {
-                if (dir == Direction.Left)
-                {
-                    return new Point(TilePosition.X + 1, TilePosition.Y);
-                    
-                }
-                else if (dir == Direction.Right)
-                {
-                    return new Point(TilePosition.X - 1, TilePosition.Y);
-                }
-                else if (dir == Direction.Up)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y + 1);
-                }
-                else if (dir == Direction.Down)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y - 1);
-                }
-            }
-            else if(faceDirection == Direction.Left)
-            {
-                if (dir == Direction.Left)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y + 1);
-                }
-                else if (dir == Direction.Right)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y - 1);
-                    
-                }
-                else if (dir == Direction.Up)
-                {
-                    return new Point(TilePosition.X - 1, TilePosition.Y);
-                    
-                }
-                else if (dir == Direction.Down)
-                {
-                    return new Point(TilePosition.X + 1, TilePosition.Y);
-                }
-            }
-            else if(faceDirection == Direction.Right)
-            {
-                if (dir == Direction.Left)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y - 1);
-                }
-                else if (dir == Direction.Right)
-                {
-                    return new Point(TilePosition.X, TilePosition.Y + 1);
-                }
-                else if (dir == Direction.Up)
-                {
-                    return new Point(TilePosition.X + 1, TilePosition.Y);
-                }
-                else if (dir == Direction.Down)
-                {
-                    return new Point(TilePosition.X - 1, TilePosition.Y);
-                }
-            }
-            return new Point(0, 0);
-            
-        }
         public override void Step(bool[,] obstacle)
         {
-            var primTarget = unit(Direction.Left);
-            var secTarget = unit(Direction.Up);
-            if (!obstacle[(int)primTarget.X, (int)primTarget.Y])
-            {
-                faceDirection = GetLeft();
-                move(primTarget);
-            }
-            else if(!obstacle[(int)secTarget.X, (int)secTarget.Y])
-            {
-                move(secTarget);
-            }
-            else
-            {
-                faceDirection = GetRight();
-            }
+            var primTarget = calcUnit(Direction.Left);
+            stepping(obstacle, primTarget);
         }
+        
 
-        private void move(Point p)
-        {
-            TileOldPosition = TilePosition;
-            TilePosition.X = p.X;
-            TilePosition.Y = p.Y;
-            
-        } 
     }
 }
