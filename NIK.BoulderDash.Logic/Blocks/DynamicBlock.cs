0caf04f50af5d69bbfff11cd90a75791bb9bad7c
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 15:24:12 +0200

    add explosive property for later usage

--- NIK.BoulderDash.Logic/Blocks/DynamicBlock.cs
+++ NIK.BoulderDash.Logic/Blocks/DynamicBlock.cs
@@ -12,6 +12,10 @@ namespace NIK.BoulderDash.Logic.Blocks
         private Point tilePosition;
         public ref Point TilePosition { get => ref tilePosition; }
 
+        public DynamicBlock(bool explosive, bool isRounded) :base(isRounded)
+        {
+            this.Explosive = explosive;
+        }
         private Point tileOldPosition;
         public ref Point TileOldPosition { get => ref tileOldPosition; }
 
