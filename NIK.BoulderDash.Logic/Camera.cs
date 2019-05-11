57f4291e6244c3f4776ec760cdeb8d3ae6655815
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-11 15:39:48 +0200

    fix camera follow, not 100% but ok

--- NIK.BoulderDash.Logic/Camera.cs
+++ NIK.BoulderDash.Logic/Camera.cs
@@ -9,40 +9,70 @@ namespace NIK.BoulderDash.Logic
 {
     public class Camera
     {
-        public int Width { get; } = 20;
-        public int Height { get;  } = 12;
+        public bool isInStage(Point p)
+        {
+            if(p.X+6 > (center.X-AngleWidthTile/2) && p.X-5 < (center.X + AngleWidthTile / 2) )
+            {
+                if(p.Y+2 > (center.Y - AngleHeightTile-2 / 2)  && p.Y-2 < (center.Y + AngleHeightTile / 2) )
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public Camera()
+        {
+            //center = new Point(-AngleWidthTile, -AngleHeightTile);
+        }
+        public int AngleWidthTile { get; } = 20;
+        public int AngleHeightTile { get;  } = 12;
         private Point center;
         private Point centerOld;
         public ref Point Center { get => ref center; }
         public ref Point CenterOld { get => ref centerOld; }
 
+        bool first = true;
         public void Follow(Point target)
         {
-            CenterOld = center;
 
-            //if (target.X > (Width - centerOld.X)%6)
-            //{
-            //    center.X += 4;
-            //}
+            centerOld = center;
 
-
-            if (Math.Sqrt(Math.Pow(center.X - target.X, 2) + Math.Pow(center.Y - target.Y, 2)) > 5 || (target.X < 7 || target.Y < 7))
+            if (target.X < AngleWidthTile / 2)
             {
-                target.X *= target.X / 15;
+                target.X = AngleWidthTile / 2;
+            }
+            if (target.X > AngleWidthTile * 1.5)
+            {
+                target.X = AngleWidthTile * 1.5;
+            }
+            if (target.Y < AngleHeightTile / 2)
+            {
+                target.Y = AngleHeightTile / 2;
+            }
+            if (target.Y > AngleHeightTile * 1.5 - 2)
+            {
+                target.Y = AngleHeightTile * 1.5 - 2;
+            }
 
-                if (target.X >= Width * 2)
-                    target.X = Width + Width - 1;
 
-                target.Y *= target.Y / 7;
+            if (first)
+            {
+                Center = target;
+                CenterOld = target;
+                first = false;
+            }
 
-                if (target.Y >= Height * 2)
-                    target.Y = Height + Height - 4;
+           
+            if (Math.Abs(Center.X - target.X) >= 5)
+            {
+                Center.X = target.X;
+            }
 
-                center = target;
+            if (Math.Abs(Center.Y - target.Y) >= 2)
+            {
+                Center.Y = target.Y;
             }
 
         }
-    }
-
-    
+    }   
 }
