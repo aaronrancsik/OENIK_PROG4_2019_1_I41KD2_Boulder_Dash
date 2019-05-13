cdf98bf8b98807cd03b49a6ad9052eff912565e7
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-13 16:03:22 +0200

    add main menu

---
+++ NIK.BoulderDash.UI/MianMenu.xaml.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="MianMenu.xaml.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace NIK.BoulderDash.UI
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for MianMenu.xaml.
+    /// </summary>
+    public partial class MianMenu : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MianMenu"/>  class.
+        /// This is the main menu.
+        /// </summary>
+        public MianMenu()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
