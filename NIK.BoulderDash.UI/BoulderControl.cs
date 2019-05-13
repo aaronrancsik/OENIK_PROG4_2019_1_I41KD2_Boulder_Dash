a5e05ec9c0392d1652f06ad374f37b4e0661e661
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-13 04:13:41 +0200

    fix map

--- NIK.BoulderDash.UI/BoulderControl.cs
+++ NIK.BoulderDash.UI/BoulderControl.cs
@@ -74,7 +74,7 @@ namespace NIK.BoulderDash.UI
             }
 
             this.logic = new GameLogic();
-            this.model = this.logic.LoadLevel(Properties.Resources.AL10);
+            this.model = this.logic.LoadLevel(Properties.Resources.AL14);
             this.display = new BoulderDisplay(this.model, this.ActualWidth, this.ActualHeight, GameModel.MOVETIME, this.animatedVisualBrushes);
             this.InvalidateVisual();
         }
