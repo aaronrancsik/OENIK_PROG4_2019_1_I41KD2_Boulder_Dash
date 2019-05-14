ce488afe126ea455b5df1d68a20a2d3f4edb8953
Aaron Rancsik <20153302+aaronrancsik@users.noreply.github.com>
2019-05-14 13:46:40 +0200

    restart and back to menu now working

--- NIK.BoulderDash.UI/MainWindow.xaml.cs
+++ NIK.BoulderDash.UI/MainWindow.xaml.cs
@@ -5,12 +5,15 @@
 namespace NIK.BoulderDash.UI
 {
     using System.Windows;
+    using System.Windows.Input;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml.
     /// </summary>
     public partial class MainWindow : Window
     {
+        private byte[] map;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// </summary>
@@ -20,10 +23,45 @@ namespace NIK.BoulderDash.UI
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Loads the selected map.
+        /// </summary>
+        /// <param name="map">The map.</param>
+        public void LoadMap(byte[] map)
+        {
+            this.map = map;
+            (this.FindName("control") as BoulderControl).LoadMap(map);
+        }
+
+        /// <summary>
+        /// Handles the Loaded event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             (sender as Window).WindowState = WindowState.Normal;
             (sender as Window).Width = ((sender as Window).ActualHeight / 12) * 20;
+        }
+
+        /// <summary>
+        /// Handles the PreviewKeyDown event of the Window control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="KeyEventArgs"/> instance containing the event data.</param>
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                MainMenu menu = new MainMenu();
+                menu.Show();
+                this.Close();
+            }
+
+            if (e.Key == Key.Space)
+            {
+                (this.FindName("control") as BoulderControl).Restart();
+            }
         }
     }
 }
